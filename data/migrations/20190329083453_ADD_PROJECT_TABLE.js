45f0ad4211d681a558a4bcc0b00fa34c4c7c5699
conconrob <connortprobertson@gmail.com>
2019-03-29 08:40:23 +0000

    schema for projects done

---
+++ data/migrations/20190329083453_ADD_PROJECT_TABLE.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("projects", function(table) {
+    table.increments();
+    table
+      .string("name", 120)
+      .notNullable()
+      .unique();
+    table.string("description", 255);
+    table.boolean("completed").defaultsTo(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("projects");
+};
